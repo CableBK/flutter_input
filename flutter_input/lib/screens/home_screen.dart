787951f8abf128fb43321fe4c51371b2f31e58b1
เคเบิล <cbkcontactmail@gmail.com>
2024-07-20 21:38:15 +0700

    Update TextField

---
+++ flutter_input/lib/screens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black26,
+        title: Text('ลงทะเบียน'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              buildTextField(),
+              ElevatedButton(
+                  onPressed: () {
+                    print(
+                        'Name = ${nameController.text} ${surnameController.text}');
+                  },
+                  child: Text('บันทึก'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField() => Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: 'ชื่อ'),
+            maxLength: 50,
+            keyboardType: TextInputType.name,
+            controller: nameController,
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'นามสกุล'),
+            maxLength: 50,
+            keyboardType: TextInputType.name,
+            controller: surnameController,
+          ),
+        ],
+      );
+}
